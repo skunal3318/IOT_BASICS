0f3b9b7a1f835b7360a3aae75eea85ec4707414c
Kunal Yadav <kunal780954@gmail.com>
2025-05-28 13:38:28 +0530

    Update code.ino

--- Led_Blink_Circuit/code.ino
+++ Led_Blink_Circuit/code.ino
@@ -1,1 +1,10 @@
+void setup(){
+  pinMode(13,OUTPUT); //Set Pin 13 as the output
+}
 
+void loop(){
+  digitalWrite(13,HIGH); // Turn LED on
+  delay(1000);           //Wait 1 second
+  digitalWrite(13,LOW);  //Turn LED off
+  delay(1000);           //Wait 1 second
+}
