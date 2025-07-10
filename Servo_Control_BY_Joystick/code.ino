bf9a2ec483d33e3b0a1cfdfe296d50db3a2a5520
Kunal Yadav <kunal780954@gmail.com>
2025-07-10 17:58:56 +0530

    Create code.ino

---
+++ Servo_Control_BY_Joystick/code.ino
@@ -0,0 +1,58 @@
+#include <Servo.h>
+int sxpin = A2;
+int sypin = A3;
+int sxval;
+int syval;
+int spin = 7;
+int buzzer=2;
+int jbuttonval;
+int xservo = 9;
+int yservo = 10;
+int xpos;
+int ypos;
+int dt = 250;
+Servo xdir;
+Servo ydir;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sxpin , INPUT);
+  pinMode(sypin , INPUT);
+  pinMode(spin , OUTPUT);
+  pinMode(buzzer , OUTPUT);
+  xdir.attach(xservo);
+  ydir.attach(yservo);
+  digitalWrite(spin, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sxval = analogRead(sxpin);
+  xpos = (180./1023.)*(sxval);
+
+  syval = analogRead(sypin);
+  ypos = (180./1023.)*(syval);
+
+  jbuttonval = digitalRead(spin);
+  if(jbuttonval == 0){
+    pinMode(buzzer, HIGH);
+  }else{
+    pinMode(buzzer, LOW);
+  }
+
+  Serial.print("X val :: ");
+  Serial.print(sxval);
+  Serial.print(" , ");
+  Serial.print("Y val :: ");
+  Serial.print(syval);
+  Serial.print(" , ");
+  Serial.print("Jbutton val :: ");
+  Serial.println(jbuttonval);
+
+  xdir.write(xpos);
+  ydir.write(ypos);
+
+  delay(dt);
+}
