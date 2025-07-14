e88d937bd9a7b26b28acccf3eed28a342d3701d8
Kunal Yadav <kunal780954@gmail.com>
2025-07-15 00:28:01 +0530

    Create code.ino

---
+++ 74HC595/Bit Flipper/Using the tilde symbol/code.ino
@@ -0,0 +1,20 @@
+int latchpin = 11;
+int clockpin = 9;
+int datapin = 12;
+byte LEDs = 0b10000000;
+  
+void setup(){
+  Serial.begin(9600);
+  pinMode(latchpin, OUTPUT);
+  pinMode(clockpin, OUTPUT);
+  pinMode(datapin, OUTPUT);
+}
+
+void loop(){
+  Serial.println(LEDs, BIN);
+  digitalWrite(latchpin, LOW);
+  shiftOut(datapin, clockpin, LSBFIRST, LEDs);
+  digitalWrite(latchpin, HIGH);
+  delay(300);
+  LEDs = ~LEDs;
+  }
